16ab5bff87eb146d027e89e9a2a7bd6d046f6e4c
JCharles246 <jose.munoz34@uabc.edu.mx>
2019-11-03 01:11:20 -0700

    Pluggin
    Added Typed js Plugging and modified index.html & PageStyles.css

--- Js/Script.js
+++ Js/Script.js
@@ -7,4 +7,13 @@ $(document).ready(function()
 			play: 5000,
 			pagination: false
 		});
+
+	var typed = new Typed(".typed", 
+	{
+		strings: ["Computer Science Engineer", "Game Programmer and Developer", "3D Designer"],
+		typeSpeed: 50,
+		loop: true,
+		starDelay: 1000,
+		showCursor: true
+	})
 });
