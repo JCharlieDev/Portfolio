e07b0a2a3661e1d351fb5d716c135e4fbff02f57
JCharles246 <jose.munoz34@uabc.edu.mx>
2019-11-16 21:27:21 -0800

    Finish
    Finished main site.

--- Js/Script.js
+++ Js/Script.js
@@ -1,3 +1,22 @@
+$(window).on("load", function()
+{
+	$(".loader .inner").fadeOut(3000, function()
+	{
+		$(".loader").fadeOut(500);
+	});
+
+		$(".items").isotope(
+	{
+		filter: '*',
+		animationOptions: 
+		{
+			duration: 1500,
+			easing: "linear",
+			queue: false
+		}
+	})
+})
+
 //	Will ececute the function when the document is ready.
 $(document).ready(function()
 {
@@ -57,17 +76,6 @@ $(document).ready(function()
 
 	$("[data-fancy] ").fancybox();
 
-	$(".items").isotope(
-	{
-		filter: '*',
-		animationOptions: 
-		{
-			duration: 1500,
-			easing: "linear",
-			queue: false
-		}
-	})
-
 	$("#filters a").click(function()
 	{
 		$("#filters .current").removeClass("current");
@@ -90,7 +98,36 @@ $(document).ready(function()
 
 	});
 
+	$("#navigation li a").click(function(e)
+	{
+		e.preventDefault();
 
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+
+		$("html, body").animate({scrollTop: targetPosition - 50}, "slow");
+	});
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation()
+	{
+		var body = $("body");
+
+		if($(window).scrollTop() >= navTop)
+		{
+			body.css("padding-top", nav.outerHeight() + "px")
+			body.addClass("fixedNav");
+		}
+		else
+		{
+			body.css("padding-top", 0)
+			body.removeClass("fixedNav");
+		}
+	}
 
 });
 
